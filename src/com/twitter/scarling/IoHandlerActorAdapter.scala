f94738084aeef46300af0a30a8e787a365b4d8b6
Robey Pointer <robey@twitter.com>
2008-06-18 18:12:32 -0400

    log exceptions that aren't associated with a session.

--- src/com/twitter/scarling/IoHandlerActorAdapter.scala
+++ src/com/twitter/scarling/IoHandlerActorAdapter.scala
@@ -2,6 +2,7 @@ package com.twitter.scarling
 
 import scala.actors.Actor
 import org.apache.mina.common._
+import net.lag.logging.Logger
 
 
 // Actor messages for the Mina "events"
@@ -17,19 +18,31 @@ object MinaMessage {
 
 
 class IoHandlerActorAdapter(val actorFactory: (IoSession) => Actor) extends IoHandler {
+
+    private val log = Logger.get
     private val ACTOR_KEY = "scala.mina.actor"
 
     private def actorFor(session: IoSession) = session.getAttribute(ACTOR_KEY).asInstanceOf[Actor]
-    
+
     def sessionCreated(session: IoSession) = {
         val actor = actorFactory(session)
         session.setAttribute(ACTOR_KEY, actor)
     }
-    
+
     def sessionOpened(session: IoSession) = actorFor(session) ! MinaMessage.SessionOpened
     def messageReceived(session: IoSession, message: AnyRef) = actorFor(session) ! new MinaMessage.MessageReceived(message)
     def messageSent(session: IoSession, message: AnyRef) = actorFor(session) ! new MinaMessage.MessageSent(message)
-    def exceptionCaught(session: IoSession, cause: Throwable) = actorFor(session) ! new MinaMessage.ExceptionCaught(cause)
+
+    def exceptionCaught(session: IoSession, cause: Throwable) = {
+        actorFor(session) match {
+            case null => {
+                // weird bad: an exception happened but i guess it wasn't associated with any existing session.
+                log.error(cause, "Exception inside mina!")
+            }
+            case actor: Actor => actor ! new MinaMessage.ExceptionCaught(cause)
+        }
+    }
+
     def sessionIdle(session: IoSession, status: IdleStatus) = actorFor(session) ! new MinaMessage.SessionIdle(status)
 
     def sessionClosed(session: IoSession) = {
