938c00ad439bf5acdba9c4ebfdf519a333878676
Robey Pointer <robey@twitter.com>
2008-11-19 10:40:50 -0800

    oops!

---
+++ src/main/scala/com/twitter/scarling/Time.scala
@@ -0,0 +1,10 @@
+package com.twitter.scarling
+
+// stolen from nick's ruby version.
+// this lets unit tests muck around with temporality without calling Thread.sleep().
+object Time {
+  private var offset: Long = 0
+
+  final def now() = System.currentTimeMillis + offset
+  final def advance(msec: Int) = offset += msec
+}
