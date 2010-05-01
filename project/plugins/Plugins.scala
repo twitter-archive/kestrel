925b4f117f14b1c0d2ca77f58f04ed9da9585a6e
Robey Pointer <robey@twitter.com>
2010-05-01 16:52:18 -0700

    get standard-project from twitter nest.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val twitterNest = "com.twitter" at "http://www.lag.net/nest"
   val defaultProject = "com.twitter" % "standard-project" % "0.1"
 }
