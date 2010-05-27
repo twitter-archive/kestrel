7a9e4b14af7ab29ae6a4b6935d3dd4e1f6391afd
Robey Pointer <robey@twitter.com>
2010-05-27 15:17:05 -0700

    update shell script version.

--- src/scripts/kestrel.sh
+++ src/scripts/kestrel.sh
@@ -7,7 +7,7 @@
 #   /var/log/$APP_NAME (chown daemon, chmod 775)
 
 APP_NAME="kestrel"
-VERSION="1.2"
+VERSION="1.2.1"
 APP_HOME="/opt/local/$APP_NAME/current"
 AS_USER="daemon"
 DAEMON="/usr/local/bin/daemon"
