c8e1afc5d6d21cf211daaf6a4144bb65bb14483e
Robey Pointer <robey@twitter.com>
2010-07-28 12:55:17 -0700

    changes for sbt.

--- project/build/KestrelProject.scala
+++ project/build/KestrelProject.scala
@@ -8,10 +8,12 @@ class KestrelProject(info: ProjectInfo) extends StandardProject(info) {
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
 
   val configgy = "net.lag" % "configgy" % "1.5.3"
-  val naggati = "net.lag" % "naggati" % "0.7.2"
+  val naggati = "net.lag" % "naggati" % "0.7.2a"
   val mina = "org.apache.mina" % "mina-core" % "2.0.0-M6"
   val slf4j_api = "org.slf4j" % "slf4j-api" % "1.5.2"
   val slf4j_jdk14 = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
+
+  val twitter_actors = "com.twitter" % "twitteractors" % "1.0.0"
 
   override def mainClass = Some("net.lag.kestrel.Kestrel")
 
