079682b258a31935586156dbc0a1ef8db02e139a
Robey Pointer <robey@twitter.com>
2010-09-15 11:40:56 -0700

    fix plugins.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val scalaTools = "nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  val scalaTools = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
+  val lagNet = "twitter.com" at "http://www.lag.net/repo/"
   val defaultProject = "com.twitter" % "standard-project" % "0.7.1"
 }
