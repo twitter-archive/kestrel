18b7a70092f07de9c9ac03152ca07dcdb601d1a4
Robey Pointer <robey@twitter.com>
2010-10-17 13:58:46 -0700

    update plugin and project file.

--- project/build/KestrelProject.scala
+++ project/build/KestrelProject.scala
@@ -1,17 +1,16 @@
 import sbt._
-import com.twitter.sbt.StandardProject
+import com.twitter.sbt._
 
-class KestrelProject(info: ProjectInfo) extends StandardProject(info) {
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
-  val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.7"
-  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
-
-  val configgy = "net.lag" % "configgy" % "1.5.3"
+class KestrelProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
+  val configgy = "net.lag" % "configgy" % "1.6.4"
   val naggati = "net.lag" % "naggati" % "0.7.3"
   val twitterActors = "com.twitter" % "twitteractors" % "1.0.0"
   val mina = "org.apache.mina" % "mina-core" % "2.0.0-M6"
   val slf4j_api = "org.slf4j" % "slf4j-api" % "1.5.2"
   val slf4j_jdk14 = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
+  val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.7"
+
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
 
   override def mainClass = Some("net.lag.kestrel.Kestrel")
 
@@ -23,7 +22,8 @@ class KestrelProject(info: ProjectInfo) extends StandardProject(info) {
         <distribution>repo</distribution>
       </license>
     </licenses>
-  val publishTo = Resolver.sftp("green.lag.net", "green.lag.net", "/web/repo")
 
   override def releaseBuild = true
+
+  override def subversionRepository = Some("http://svn.local.twitter.com/maven-public")
 }
