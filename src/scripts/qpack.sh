e966b0b021409abad89680d71526c10fc16ca0a7
Robey Pointer <robey@twitter.com>
2010-10-27 18:20:30 -0700

    update scripts for new scala.

--- src/scripts/qpack.sh
+++ src/scripts/qpack.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DIST_HOME="$(dirname $0)/.."
-java -server -classpath @DIST_CLASSPATH@ scala.tools.nsc.MainGenericRunner net.lag.kestrel.tools.QPacker "$@"
+java -server -classpath @DIST_CLASSPATH@ net.lag.kestrel.tools.QPacker "$@"
