24d332fd44f794195d434ecf63246137bb6c68f2
Robey Pointer <robey@twitter.com>
2010-12-25 15:25:03 -0500

    tests for TextHandler.

---
+++ src/test/scala/net/lag/kestrel/TextHandlerSpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2010 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.lag.kestrel
+
+import com.twitter.naggati.test.TestCodec
+import org.jboss.netty.buffer.ChannelBuffers
+import org.specs.Specification
+
+class TextHandlerSpec extends Specification {
+  def wrap(s: String) = ChannelBuffers.wrappedBuffer(s.getBytes)
+
+  "TextCodec" should {
+    "get request" in {
+      val codec = new TestCodec(TextCodec.read, TextCodec.write)
+
+      codec(wrap("get foo\r\n")) mustEqual List(TextRequest("get", List("foo"), Nil))
+      codec(wrap("get f")) mustEqual Nil
+      codec(wrap("oo\r\n")) mustEqual List(TextRequest("get", List("foo"), Nil))
+
+      codec(wrap("get foo 100")) mustEqual Nil
+      codec(wrap("\n")) mustEqual List(TextRequest("get", List("foo", "100"), Nil))
+    }
+
+    "put request" in {
+      val codec = new TestCodec(TextCodec.read, TextCodec.write)
+
+      codec(wrap("put foo:\n")) mustEqual Nil
+      codec(wrap("hello\n")) mustEqual Nil
+      val stream = codec(wrap("\n"))
+      stream.size mustEqual 1
+      val r = stream(0).asInstanceOf[TextRequest]
+      r.command mustEqual "put"
+      r.args mustEqual List("foo")
+      r.items.map { new String(_) } mustEqual List("hello")
+    }
+
+    "quit request" in {
+      val codec = new TestCodec(TextCodec.read, TextCodec.write)
+      codec(wrap("QUIT\r\n")) mustEqual List(TextRequest("quit", Nil, Nil))
+    }
+
+    "success response" in {
+      val codec = new TestCodec(TextCodec.read, TextCodec.write)
+      codec.send(CountResponse(23)) mustEqual List("+23\n")
+    }
+
+    "error response" in {
+      val codec = new TestCodec(TextCodec.read, TextCodec.write)
+      codec.send(ErrorResponse("Bad karma")) mustEqual List("-Bad karma\n")
+    }
+
+    "empty response" in {
+      val codec = new TestCodec(TextCodec.read, TextCodec.write)
+      codec.send(ItemResponse(None)) mustEqual List("*\n")
+    }
+
+    "item response" in {
+      val codec = new TestCodec(TextCodec.read, TextCodec.write)
+      codec.send(ItemResponse(Some("hello".getBytes))) mustEqual List(":hello\n")
+    }
+  }
+}
