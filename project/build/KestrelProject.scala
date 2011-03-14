4440577858c9633495ac76c7b53e9c83c4317fab
Robey Pointer <robey@twitter.com>
2011-03-14 15:31:51 -0700

    upgrade to a version of xrayspecs that works with mave

--- project/build/KestrelProject.scala
+++ project/build/KestrelProject.scala
@@ -8,7 +8,7 @@ class KestrelProject(info: ProjectInfo) extends StandardProject(info) with Subve
   val mina = "org.apache.mina" % "mina-core" % "2.0.0-M6"
   val slf4j_api = "org.slf4j" % "slf4j-api" % "1.5.2"
   val slf4j_jdk14 = "org.slf4j" % "slf4j-jdk14" % "1.5.2"
-  val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.7"
+  val xrayspecs = "com.twitter" % "xrayspecs_2.7.7" % "2.1.2"
 
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
 
