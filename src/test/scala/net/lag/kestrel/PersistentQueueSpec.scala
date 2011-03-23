508a12616381311a50c10f9f3a0346da58eef6c7
Robey Pointer <robey@twitter.com>
2011-03-23 14:07:58 -0700

    factor out DumpJournal.

--- src/test/scala/net/lag/kestrel/PersistentQueueSpec.scala
+++ src/test/scala/net/lag/kestrel/PersistentQueueSpec.scala
@@ -27,25 +27,12 @@ import org.specs.Specification
 import org.specs.matcher.Matcher
 import config._
 
-class PersistentQueueSpec extends Specification with TempFolder with TestLogging with QueueMatchers {
-  def dumpJournal(qname: String): String = {
-    var rv = new mutable.ListBuffer[JournalItem]
-    new Journal(new File(folderName, qname).getCanonicalPath).replay(qname) { item => rv += item }
-    rv map {
-      case JournalItem.Add(item) =>
-        if (item.data.size > 0 && item.data(0) > 0) {
-          "add(%d:%d:%s)".format(item.data.size, item.xid, new String(item.data))
-        } else {
-          "add(%d:%d)".format(item.data.size, item.xid)
-        }
-      case JournalItem.Remove => "remove"
-      case JournalItem.RemoveTentative => "remove-tentative"
-      case JournalItem.SavedXid(xid) => "xid(%d)".format(xid)
-      case JournalItem.Unremove(xid) => "unremove(%d)".format(xid)
-      case JournalItem.ConfirmRemove(xid) => "confirm-remove(%d)".format(xid)
-    } mkString ", "
-  }
-
+class PersistentQueueSpec extends Specification
+  with TempFolder
+  with TestLogging
+  with QueueMatchers
+  with DumpJournal
+{
   "PersistentQueue" should {
     val timer = new FakeTimer()
 
