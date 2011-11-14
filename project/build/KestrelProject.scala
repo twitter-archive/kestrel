c92f0cfba80f70ad59e8c5a3fe07b9a03bfd32e6
Robey Pointer <robey@twitter.com>
2011-11-14 17:41:52 -0500

    move version to 2.2.0-SNAPSHOT

--- project/build/KestrelProject.scala
+++ project/build/KestrelProject.scala
@@ -11,8 +11,8 @@ class KestrelProject(info: ProjectInfo)
 {
   val ostrich = "com.twitter" % "ostrich" % "4.9.4"
   val naggati = "com.twitter" % "naggati" % "2.2.0"
-  val finagle = "com.twitter" % "finagle-core" % "1.9.4-SNAPSHOT"
-  val finagle_ostrich4 = "com.twitter" % "finagle-ostrich4" % "1.9.0"
+  val finagle = "com.twitter" % "finagle-core" % "1.9.5"
+  val finagle_ostrich4 = "com.twitter" % "finagle-ostrich4" % "1.9.5"
   val scrooge_runtime = "com.twitter" % "scrooge-runtime" % "1.0.3"
   override def scroogeVersion = "1.1.7"
 
