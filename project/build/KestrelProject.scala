aab2ccf8ddcf83c1ce400ca1321980fc6f6ae4cd
Stephan Zuercher <stephan@twitter.com>
2012-02-06 16:49:59 -0800

    upgrade to ostrich 4.10.4 (fix dependency/compile errors)

--- project/build/KestrelProject.scala
+++ project/build/KestrelProject.scala
@@ -15,7 +15,7 @@ class KestrelProject(info: ProjectInfo)
   val naggati = "com.twitter" % "naggati" % "2.2.2-SNAPSHOT" intransitive() // allow custom netty
   val netty   = "org.jboss.netty" % "netty" % "3.2.6.Final"
   val finagle = "com.twitter" % "finagle-core" % finagleVersion
-  val ostrich = "com.twitter" % "ostrich" % "4.10.1-SNAPSHOT"
+  val ostrich = "com.twitter" % "ostrich" % "4.10.4"
   val finagle_ostrich4 = "com.twitter" % "finagle-ostrich4" % finagleVersion
 
   val scrooge_runtime = "com.twitter" % "scrooge-runtime" % "1.0.3"
