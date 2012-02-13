6901eb3eddc5542fc015ac8025e2b6c44e948314
Robey Pointer <robey@twitter.com>
2012-02-13 14:20:22 -0800

    fix devel.sh

--- src/scripts/devel.sh
+++ src/scripts/devel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Starting kestrel in development mode..."
-java -server -Xmx1024m -Dstage=development -jar ./dist/kestrel/kestrel-@VERSION@.jar
+DIST_HOME="$(dirname $(readlink $0 || echo $0))/../.."
+java -server -Xmx1024m -Dstage=development -jar $DIST_HOME/kestrel/kestrel-@VERSION@.jar
 
