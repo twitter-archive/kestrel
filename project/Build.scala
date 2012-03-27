0e9de4bfa8238a99cf8b86e57b924f17c8f5e8fe
Robey Pointer <robey@twitter.com>
2012-03-27 14:42:14 -0700

    clean up with new knowledge from libkestrel

--- project/Build.scala
+++ project/Build.scala
@@ -5,49 +5,44 @@ import com.twitter.sbt._
 object Kestrel extends Build {
   val finagleVersion = "1.11.1"
 
-  override lazy val settings = super.settings ++
-    Seq(
-      name := "kestrel",
-      organization := "net.lag",
-      version := "2.2.0-SNAPSHOT",
-      scalaVersion := "2.9.1",
-
-      // time-based tests cannot be run in parallel
-      logBuffered in Test := false,
-      parallelExecution in Test := false,
-
-      libraryDependencies ++= Seq(
-        "com.twitter" %% "ostrich" % "4.10.6",
-        "com.twitter" %% "naggati" % "2.2.3" intransitive(), // allow custom netty
-        "com.twitter" %% "finagle-core" % finagleVersion,
-        "com.twitter" %% "finagle-ostrich4" % finagleVersion,
-        "org.jboss.netty" % "netty" % "3.2.6.Final",
-        "com.twitter" %% "scrooge-runtime" % "1.1.3",
-
-        // for tests only:
-        "org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
-        "org.jmock" % "jmock" % "2.4.0" % "test",
-        "cglib" % "cglib" % "2.1_3" % "test",
-        "asm" % "asm" % "1.5.3" % "test",
-        "org.objenesis" % "objenesis" % "1.1" % "test",
-        "org.hamcrest" % "hamcrest-all" % "1.1" % "test"
-      ),
-
-      mainClass := Some("net.lag.kestrel.Kestrel")
-    )
-
   lazy val root = Project(
     id = "kestrel",
     base = file("."),
     settings = Project.defaultSettings ++
       StandardProject.newSettings ++
       SubversionPublisher.newSettings ++
-      CompileThriftScrooge.newSettings ++ Seq(
-        // any settings that override defaults have to go here. :/
-        CompileThriftScrooge.scroogeVersion := "1.1.7",
-        PackageDist.packageDistConfigFilesValidationRegex := Some(".*"),
-        SubversionPublisher.subversionRepository := Some("https://svn.twitter.biz/maven-public"),
-        publishArtifact in Test := true
-      )
+      CompileThriftScrooge.newSettings
+  ).settings(
+    name := "kestrel",
+    organization := "net.lag",
+    version := "2.2.0-SNAPSHOT",
+    scalaVersion := "2.9.1",
+
+    // time-based tests cannot be run in parallel
+    logBuffered in Test := false,
+    parallelExecution in Test := false,
+
+    libraryDependencies ++= Seq(
+      "com.twitter" %% "ostrich" % "4.10.6",
+      "com.twitter" %% "naggati" % "2.2.3" intransitive(), // allow custom netty
+      "com.twitter" %% "finagle-core" % finagleVersion,
+      "com.twitter" %% "finagle-ostrich4" % finagleVersion,
+      "org.jboss.netty" % "netty" % "3.2.6.Final",
+      "com.twitter" %% "scrooge-runtime" % "1.1.3",
+
+      // for tests only:
+      "org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
+      "org.jmock" % "jmock" % "2.4.0" % "test",
+      "cglib" % "cglib" % "2.1_3" % "test",
+      "asm" % "asm" % "1.5.3" % "test",
+      "org.objenesis" % "objenesis" % "1.1" % "test",
+      "org.hamcrest" % "hamcrest-all" % "1.1" % "test"
+    ),
+
+    mainClass := Some("net.lag.kestrel.Kestrel"),
+    CompileThriftScrooge.scroogeVersion := "1.1.7",
+    PackageDist.packageDistConfigFilesValidationRegex := Some(".*"),
+    SubversionPublisher.subversionRepository := Some("https://svn.twitter.biz/maven-public"),
+    publishArtifact in Test := true
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,24 @@
+sbtResolver <<= (sbtResolver) { r =>
+  Option(System.getenv("SBT_PROXY_REPO")) map { x =>
+    Resolver.url("proxy repo for sbt", url(x))(Resolver.ivyStylePatterns)                                             
+  } getOrElse r
+}
 
-resolvers += "local" at "file:/Users/robey/.m2/repo/"
+resolvers <<= (resolvers) { r =>
+  (Option(System.getenv("SBT_PROXY_REPO")) map { url =>
+    Seq("proxy-repo" at url)
+  } getOrElse {
+    r ++ Seq(
+      "twitter.com" at "http://maven.twttr.com/",
+      "scala-tools" at "http://scala-tools.org/repo-releases/",
+      "maven" at "http://repo1.maven.org/maven2/",
+      "freemarker" at "http://freemarker.sourceforge.net/maven2/"
+    )
+  }) ++ Seq("local" at ("file:" + System.getProperty("user.home") + "/.m2/repo/"))
+}
 
-resolvers += "twitter" at "http://artifactory.local.twitter.com/repo"
+externalResolvers <<= (resolvers) map identity
 
-addSbtPlugin("com.twitter" % "standard-project2" % "0.0.4-SNAPSHOT")
+addSbtPlugin("com.twitter" % "sbt-package-dist" % "0.99.0")
 
 addSbtPlugin("com.twitter" % "sbt-scrooge2" % "0.0.1")
